f1a4e3bcbaee8834d6a2abc750626e97f56a1937
Alberto Scala <31989626+albertoscala@users.noreply.github.com>
2022-11-22 11:05:12 +0100

    Update ceaser_cypher.go

--- ceaser_cypher.go
+++ ceaser_cypher.go
@@ -7,34 +7,47 @@ type CeaserCipher struct {
 	Cache string
 }
 
+func conv_char(b byte, key int) byte {
+	c := int(b)
+
+	// Uppercase chars
+	if c >= 65 && c <= 90 {
+		c += key
+		if c > 90 {
+			c = ((c - 90) + 64)
+		}
+		return byte(rune(c))
+	}
+
+	// Lowercase chars
+	if c >= 97 && c <= 122 {
+		c += key
+		if c > 122 {
+			c = ((c - 122) + 96)
+		}
+		return byte(rune(c))
+	}
+
+	return b
+}
+
 func (cc *CeaserCipher) Write(p []byte) (int, error) {
 
 	for i := 0; i < len(p); i++ {
-		c := int(p[i])
-
-		if c >= 65 && c <= 90 {
-			c += cc.Key
-			if c > 90 {
-				c = ((c - 90) + 64)
-			}
-			p[i] = byte(rune(c))
-		}
-
-		if c >= 97 && c <= 122 {
-			c += cc.Key
-			if c > 122 {
-				c = ((c - 122) + 96)
-			}
-			p[i] = byte(rune(c))
-		}
+		p[i] = conv_char(p[i], cc.Key)
 	}
 
-	fmt.Println(string(p))
+	s := string(p)
+
+	fmt.Println(s)
+	cc.Cache = s
 
 	return len(p), nil
 }
 
 func main() {
-	ceaserCipher := CeaserCipher{Key: 1}
-	fmt.Fprintf(&ceaserCipher, "zzz")
+	ceaserCipher := CeaserCipher{Key: 3}
+	fmt.Fprintf(&ceaserCipher, "tl:dr")
+	fmt.Println(ceaserCipher.Cache)
+	fmt.Fprintf(&ceaserCipher, "alberto")
 }
